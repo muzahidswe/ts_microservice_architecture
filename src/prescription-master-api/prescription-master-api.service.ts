b438b4f92d88e9c8b75b1c0db20693f349b4d033
Shahriar Islam <shahriar.islam@apsissolutions.com>
2023-02-28 13:40:19 +0600

    fix prescription image_list

--- src/prescription-master-api/prescription-master-api.service.ts
+++ src/prescription-master-api/prescription-master-api.service.ts
@@ -1,9 +1,8 @@
-import { HttpException, HttpStatus, Injectable, Logger, NotFoundException} from '@nestjs/common';
-import { CreatePrescriptionMasterApiDto } from './dto/create-prescription-master-api.dto';
-import { UpdatePrescriptionMasterApiDto } from './dto/update-prescription-master-api.dto';
+import { HttpException, HttpStatus, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { PrescriptionImageDetailsRepository } from '../database_table/repository/ms_prescription_image_details.repository';
 import { MasterPrescriptionListRepository } from 'src/database_table/repository/master-prescription-list.repository';
+import { PrescriptionImageDetailsRepository } from '../database_table/repository/ms_prescription_image_details.repository';
+import { CreatePrescriptionMasterApiDto } from './dto/create-prescription-master-api.dto';
 const fs = require('fs');
 
 @Injectable()
@@ -17,176 +16,174 @@ export class PrescriptionMasterApiService {
   ) {
   }
 
-  async create_prescription(CreatePrescriptionMasterApiDto: CreatePrescriptionMasterApiDto[]) : Promise <any>{
+  async create_prescription(CreatePrescriptionMasterApiDto: CreatePrescriptionMasterApiDto[]): Promise<any> {
     return new Promise(async (resolve, reject) => {
       this.logger.log('Adding New Prescription');
-      try{
+      try {
         const promotionId = 0;
         const apiData = CreatePrescriptionMasterApiDto[0];
-        
+
         const insertData = {
-          "professional_id" : apiData.professional_id !== undefined ? Number(apiData.professional_id) : null,
-          "created_by" : apiData.created_by !== undefined ? Number(apiData.created_by) : null,
-          "prescription_image_count" : (apiData.prescription_image_data !== undefined && apiData.prescription_image_data.length > 0) ? apiData.prescription_image_data.length : 0
+          "professional_id": apiData.professional_id !== undefined ? Number(apiData.professional_id) : null,
+          "created_by": apiData.created_by !== undefined ? Number(apiData.created_by) : null,
+          "prescription_image_count": (apiData.prescription_image_data !== undefined && apiData.prescription_image_data.length > 0) ? apiData.prescription_image_data.length : 0
         };
 
         const insertLog = await this.masterPrescriptionListRepository.insert(insertData);
         const image_path = 'storage/uploads/assets/images/prescription/';
         const base64_image = await this.base64_to_image(apiData.prescription_image_data, image_path, insertLog.identifiers[0].id, apiData.created_by);
-  
-        resolve ((base64_image == true && insertLog.identifiers[0].id > 0) ? true : false);
-      } catch(errror){
+
+        resolve((base64_image == true && insertLog.identifiers[0].id > 0) ? true : false);
+      } catch (errror) {
         console.log(errror);
-        reject (new HttpException('Forbidden', HttpStatus.FORBIDDEN));
+        reject(new HttpException('Forbidden', HttpStatus.FORBIDDEN));
       }
     });
   }
 
-  async byProfessionalPrescriptionSummary() : Promise <any>{
+  async byProfessionalPrescriptionSummary(): Promise<any> {
     return new Promise(async (resolve, reject) => {
       this.logger.log('Returning by Professional Prescription Summary');
-      try{
-          const promotionInfo = await this.masterPrescriptionListRepository
-            .createQueryBuilder('Prescription')    
-            .select([
-              'professionalInfo.category_id AS professional_category_id',
-              'Prescription.professional_id AS professional_id',
-              'professionalInfo.name AS professional_name',
-              'professionalInfo.designation AS designation',
-              'professionalInfo.department AS department',
-              'professionalInfo.organization AS organization',
-              'SUM(Prescription.prescription_image_count) AS prescription_image_count'
-            ])
-            .innerJoin('ms_professional_list', 'professionalInfo', '`professionalInfo`.`id` = `Prescription`.`professional_id`')
-            .where("Prescription.status = :status", { status: 1 })
-            .groupBy('Prescription.professional_id')
-            .orderBy('Prescription.professional_id', 'DESC')
-            .getRawMany();
+      try {
+        const promotionInfo = await this.masterPrescriptionListRepository
+          .createQueryBuilder('Prescription')
+          .select([
+            'professionalInfo.category_id AS professional_category_id',
+            'Prescription.professional_id AS professional_id',
+            'professionalInfo.name AS professional_name',
+            'professionalInfo.designation AS designation',
+            'professionalInfo.department AS department',
+            'professionalInfo.organization AS organization',
+            'SUM(Prescription.prescription_image_count) AS prescription_image_count'
+          ])
+          .innerJoin('ms_professional_list', 'professionalInfo', '`professionalInfo`.`id` = `Prescription`.`professional_id`')
+          .where("Prescription.status = :status", { status: 1 })
+          .groupBy('Prescription.professional_id')
+          .orderBy('Prescription.professional_id', 'DESC')
+          .getRawMany();
 
-          if (!promotionInfo) {
-            reject (new NotFoundException('Prescription List not found.'));
-          }
-          resolve (promotionInfo);
-      } catch(errror){
+        if (!promotionInfo) {
+          reject(new NotFoundException('Prescription List not found.'));
+        }
+        resolve(promotionInfo);
+      } catch (errror) {
         console.log(errror);
-        reject (new HttpException('Forbidden', HttpStatus.FORBIDDEN));
+        reject(new HttpException('Forbidden', HttpStatus.FORBIDDEN));
       }
     });
   }
 
-  async byProfessionalPrescriptionImageList(params) : Promise <any>{
+  async byProfessionalPrescriptionImageList(params): Promise<any> {
     return new Promise(async (resolve, reject) => {
       this.logger.log('Returning by Professional All Prescription Image List');
-      try{
-        let {professional_id, limit} = params;
+      try {
+        let { professional_id, limit } = params;
         const dateRange = await this.calculateDateRange(limit);
         const prescriptionImageList = await this.masterPrescriptionListRepository
-            .createQueryBuilder('Prescription')    
+          .createQueryBuilder('Prescription')
+          .select([
+            'PrescriptionImage.id AS image_id',
+            'Prescription.professional_id AS professional_id',
+            'PrescriptionImage.image_path AS image_path'
+          ])
+          .innerJoin('ms_prescription_image_details', 'PrescriptionImage', '`PrescriptionImage`.`prescription_id` = `Prescription`.`id`')
+          .where("Prescription.status = :status AND Prescription.professional_id = :professional_id AND PrescriptionImage.status = :extra_status", { status: 1, professional_id: professional_id, extra_status: 1 })
+          .andWhere(`DATE(Prescription.created) BETWEEN '${dateRange.start_date}' AND '${dateRange.last_date}'`)
+          .orderBy('Prescription.professional_id', 'DESC')
+          .getRawMany();
+
+        if (!prescriptionImageList) {
+          reject(new NotFoundException('Prescription Image List not found.'));
+        }
+        resolve(prescriptionImageList);
+      } catch (errror) {
+        console.log(errror);
+        reject(new HttpException('Forbidden', HttpStatus.FORBIDDEN));
+      }
+    });
+  }
+
+  async prescriptionSyncCount(user_id: number, date: string): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.logger.log('Returning Prescription Sync Count.');
+        if (user_id && date) {
+          const prescriptionList = await this.masterPrescriptionListRepository
+            .createQueryBuilder('Prescription')
             .select([
-              'PrescriptionImage.id AS image_id',
               'Prescription.professional_id AS professional_id',
-              'PrescriptionImage.image_path AS image_path'
             ])
-            .innerJoin('ms_prescription_image_details', 'PrescriptionImage', '`PrescriptionImage`.`prescription_id` = `Prescription`.`id`')
-            .where("Prescription.status = :status", { status: 1 })
-            .where("Prescription.professional_id = :professional_id", { professional_id: professional_id })
-            .where("PrescriptionImage.status = :status", { status: 1 })
-            .andWhere(`DATE(Prescription.created) BETWEEN '${dateRange.start_date}' AND '${dateRange.last_date}'`)
-            .orderBy('Prescription.professional_id', 'DESC')
+            .where("Prescription.created_by = :user_id", { user_id: user_id })
+            .andWhere("DATE(Prescription.created) = :date", { date: date })
             .getRawMany();
 
-          if (!prescriptionImageList) {
-            reject (new NotFoundException('Prescription Image List not found.'));
-          }
-          resolve (prescriptionImageList);
-      } catch(errror){
-        console.log(errror);
-        reject (new HttpException('Forbidden', HttpStatus.FORBIDDEN));
-      }
-    });
-  }
-
-  async prescriptionSyncCount(user_id: number, date: string): Promise <any> {
-    return new Promise(async (resolve, reject) => {
-      try{
-        this.logger.log('Returning Prescription Sync Count.');
-        if(user_id && date){
-          const prescriptionList = await this.masterPrescriptionListRepository
-              .createQueryBuilder('Prescription')
-              .select([
-                'Prescription.professional_id AS professional_id',    
-              ])
-              .where("Prescription.created_by = :user_id", { user_id: user_id })
-              .andWhere("DATE(Prescription.created) = :date", { date: date })
-              .getRawMany();
-        
-          if(prescriptionList.length > 0 ){
-            resolve ({success: true, msg : 'Prescription Data Count Found Successfully.', data: prescriptionList.length});
-          } else{
-            resolve ({success: false, msg : 'Prescription Data is Empty.', data: 0});
+          if (prescriptionList.length > 0) {
+            resolve({ success: true, msg: 'Prescription Data Count Found Successfully.', data: prescriptionList.length });
+          } else {
+            resolve({ success: false, msg: 'Prescription Data is Empty.', data: 0 });
           }
         } else {
-          resolve ({success: false, msg : 'API is Expecting User ID & Date.', data: 0});
+          resolve({ success: false, msg: 'API is Expecting User ID & Date.', data: 0 });
         }
-      } catch(errror){
+      } catch (errror) {
         console.log(errror);
-        reject (new HttpException('Forbidden', HttpStatus.FORBIDDEN));
+        reject(new HttpException('Forbidden', HttpStatus.FORBIDDEN));
       }
     });
   }
 
-  async base64_to_image(base64Data: string[], image_path: string, prescription_id: number, created_by: number): Promise<any>{
+  async base64_to_image(base64Data: string[], image_path: string, prescription_id: number, created_by: number): Promise<any> {
     return new Promise(async (resolve, reject) => {
       try {
         let count = 0;
         const currentdate = new Date();
         const deviceDate = currentdate.getFullYear() + "-" + ('0' + (currentdate.getMonth() + 1)).slice(-2) + "-" + ('0' + currentdate.getDate()).slice(-2);
         const path = image_path + deviceDate + '/' + prescription_id + '/';
-        if (!fs.existsSync('../' + path)){
+        if (!fs.existsSync('../' + path)) {
           fs.mkdirSync('../' + path, { recursive: true });
         }
         let imageLog = [];
         base64Data.map(imageData => {
           const base64ImageData = imageData.replace(/^data:image\/[a-z]+;base64,/, "");
           const image_path_and_name = path + (++count) + '.jpg';
-          imageLog.push({prescription_id : prescription_id, image_path : image_path_and_name, created_by: created_by});
-          require('fs').writeFile('../' + image_path_and_name, base64ImageData, 'base64', function(err) {
-            if(err == 'null') return true;
+          imageLog.push({ prescription_id: prescription_id, image_path: image_path_and_name, created_by: created_by });
+          require('fs').writeFile('../' + image_path_and_name, base64ImageData, 'base64', function (err) {
+            if (err == 'null') return true;
             else return err;
           });
         });
         await this.prescriptionImageDetailsRepository.insert(imageLog);
-        resolve (true);
+        resolve(true);
       } catch (error) {
-        reject (false);
+        reject(false);
       }
     });
   }
 
-  async calculateDateRange(limit: string){
+  async calculateDateRange(limit: string) {
     try {
       const currentdate = new Date();
       let firstday: Date, lastday: Date, firstDayRange: string, lastDayRange: string;
-      if(limit == 'all'){
+      if (limit == 'all') {
         firstday = await this.getFirstDayOfYear(2022);
         lastday = currentdate;
       }
-      else if(limit == 'week'){
+      else if (limit == 'week') {
         firstday = new Date(currentdate.setDate(currentdate.getDate() - currentdate.getDay()));
-        lastday = new Date(currentdate.setDate(currentdate.getDate() - currentdate.getDay()+6));
+        lastday = new Date(currentdate.setDate(currentdate.getDate() - currentdate.getDay() + 6));
       }
-      else if(limit == 'month'){
+      else if (limit == 'month') {
         firstday = new Date(currentdate.getFullYear(), currentdate.getMonth(), 1);
         lastday = new Date(currentdate.getFullYear(), currentdate.getMonth() + 1, 0);
       }
-      else if(limit == 'lastmonth'){
+      else if (limit == 'lastmonth') {
         firstday = new Date(currentdate.getFullYear(), currentdate.getMonth() - 1, 1);
         lastday = new Date(currentdate.getFullYear(), currentdate.getMonth() - 1 + 1, 0);
       }
-      firstDayRange = firstday.getFullYear() + "-" + ('0' + (firstday.getMonth() + 1)).slice(-2) + "-" + ('0' + firstday.getDate()).slice(-2);          
+      firstDayRange = firstday.getFullYear() + "-" + ('0' + (firstday.getMonth() + 1)).slice(-2) + "-" + ('0' + firstday.getDate()).slice(-2);
       lastDayRange = lastday.getFullYear() + "-" + ('0' + (lastday.getMonth() + 1)).slice(-2) + "-" + ('0' + lastday.getDate()).slice(-2);
-      return {"start_date": firstDayRange, "last_date" : lastDayRange};
-    } catch(errror){
+      return { "start_date": firstDayRange, "last_date": lastDayRange };
+    } catch (errror) {
       throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
     }
   }
